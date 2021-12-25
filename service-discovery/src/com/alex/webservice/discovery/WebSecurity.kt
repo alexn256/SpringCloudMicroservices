e59510290e6f25f644c993b098b767186e96f4cd
alexn256 <naumov.btm@gmail.com>
2021-12-25 19:37:53 +0200

    override source directories for discovery-service

---
+++ service-discovery/src/com/alex/webservice/discovery/WebSecurity.kt
@@ -0,0 +1,17 @@
+package com.alex.webservice.discovery
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
+
+@EnableWebSecurity
+class WebSecurity: WebSecurityConfigurerAdapter() {
+
+    override fun configure(http: HttpSecurity) {
+        http.csrf()
+            .disable()
+            .authorizeRequests()
+            .anyRequest().authenticated()
+            .and().httpBasic()
+    }
+}
